2552dac718072bbabc7d4ce8d26d006928b4e86a
Grace <carrolhn@bc.edu>
2021-02-04 22:54:20 -0500

    Completed 2.7

--- YouAreAwesome!/ViewController.swift
+++ YouAreAwesome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text=""
         
     }
 
