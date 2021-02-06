31b3129158a2c55b2adec614d5a6847e5fbe3c7d
Grace <carrolhn@bc.edu>
2021-02-06 17:12:09 -0500

    Completed 2.8

--- YouAreAwesome!/ViewController.swift
+++ YouAreAwesome!/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text=""
@@ -19,8 +21,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let daMessage = "You Are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage{
+            messageLabel.text = daMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
         
     }
     
