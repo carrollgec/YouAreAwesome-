27f68a727d4bf0238b8f061e591217c72ca5a377
Grace <carrolhn@bc.edu>
2021-02-06 17:41:04 -0500

    Completed 2.9

--- YouAreAwesome!/ViewController.swift
+++ YouAreAwesome!/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    
+    var imageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,20 +21,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let daMessage = "You Are Da Bomb!"
+       print(imageNumber)
+        //let imageName = "image" + String(imageNumber)
         
-        if messageLabel.text == awesomeMessage{
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage{
-            messageLabel.text = daMessage
-            imageView.image = UIImage(named: "image2")
-        } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10{
+            imageNumber=0
         }
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let daMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage{
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage{
+//            messageLabel.text = daMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
         
     }
     
